d94cc9c5c76b54bd34952269915672db67fd96ad
Michael <mail@michaelsimon.io>
2017-03-05 13:23:53 +0100

    person and car component and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
+import {AppComponent} from "./component/app/app.component";
+import {PersonComponent} from "./component/person/person.component";
+import {CarComponent} from "./component/car/car.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
